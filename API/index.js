4415739bfc2e44902565641e0cf3ad3ca50969c8
dann9422 <dannsk82294@gmail.com>
2021-10-26 22:55:52 -0600

    Creacion de los endpoints

--- API/index.js
+++ API/index.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require('./routes');
+const bodyParser = require('body-parser');
 
 // crear el servidor pruba
 const app = express();
+
+
+// conectar a mongo db
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/veterinaria',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useFindAndModify: false
+
+});
+
+//habilitar bodyparse
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended:true}));
+
+
+//habilitar routes 
+
+app.use('/',routes())
+
+
+
 
 // creando el puerto de arranque del servidor para subir
 app.listen(4000, () => {
