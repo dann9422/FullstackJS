9679ba08539fc43a566d7cc909b97f9fd8622f02
dannsk82294 <dannsk82294@gmail.com>
2021-10-26 19:51:37 -0600

    subiendo los cambios

--- API/index.js
+++ API/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 // crear el servidor
 const app = express();
+
+// creando el puerto de arranque del servidor para subir
+app.listen(4000, () => {
+  console.log("servidor funcionando");
+});
